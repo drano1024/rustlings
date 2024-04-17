1119c00a6b710ad0c39294d914b693525472b1f0
drano1024 <gmedrano1024@gmail.com>
2024-04-17 09:23:17 -0500

    finished chapter 3 and quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(N: i32) -> i32 {
+    if N > 40 {
+        N
+    } else {
+        2*N
+    }
+}
 
 // Don't modify this function!
 #[test]
