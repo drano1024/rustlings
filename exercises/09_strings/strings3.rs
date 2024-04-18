35f4ba72f6a0341bea86e22070156d713f782242
drano1024 <gmedrano1024@gmail.com>
2024-04-18 10:03:39 -0500

    finished chapters 8 and 9

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,38 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut s = String::new();
+    let str2rep = vec!["", " "];
+    let mut ilist: Vec<&str> = input.split("").collect();
+    let mut frontReplace: bool = true;
+    let mut backReplace: bool = true;
+    println!("{:?}", ilist);
+    while frontReplace || backReplace {
+        frontReplace = str2rep.contains(&ilist[0]);
+        backReplace = str2rep.contains(&ilist[ilist.len() - 1]);
+        if frontReplace {
+            ilist.remove(0);
+        }
+        if backReplace {
+            ilist.pop();
+        }
+    }
+    ilist.join("")
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There are multiple ways to do this!
-    ???
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut ilist: Vec<&str> = input.split(" ").collect();
+    for word in ilist.iter_mut() {
+        if *word == "cars" {
+            *word = "balloons"
+        }
+    }
+    ilist.join(" ")
 }
 
 #[cfg(test)]
@@ -39,7 +56,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
